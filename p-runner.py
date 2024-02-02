191a18eb33c128e5b1cfc34a7c7156864d2ead92
Lorelei! <17865635+Hellorelei@users.noreply.github.com>
2024-02-02 09:42:15 +0100

    Initial Git upload

---
+++ p-runner.py
@@ -0,0 +1,171 @@
+"""WowHead quest parser, by Lorelei Chevroulet:
+This script (1) iterates all text files in a given folter through a predefined
+regex filter. It (2) discards files missing specific tokens, and (3) extracts
+the desired tokens in new text files a specifified folder. 
+"""
+import sys
+import os
+from pathlib import Path
+import glob
+import time
+
+class ansi:
+    HEADER = '\033[95m'
+    BR_MAGENTA = '\033[95m'
+    BR_BLUE = '\033[94m'
+    BR_CYAN = '\033[96m'
+    BR_GREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    DIM = '\033[2m'
+    BLINK = '\033[5m'
+    UNDERLINE = '\033[4m'
+    RIGHT = '\033[C'
+    #FRAMED = '\033[52m'
+    CLEAR = '\033[J'
+    CLINE = '\033[2K'
+    UP = '\033[F'
+    DOWN = '\033[E'
+    ALLUP = '\033[H'
+    B_GREEN = '\033[42m'
+    B_RED = '\033[41m'
+    B_BLUE = '\033[44m'
+    B_MAGENTA = '\033[45m'
+    B_CYAN = '\033[46m'
+    BBR_GREEN = '\033[102m'
+    BBR_RED = '\033[101m'
+    BBR_BLUE = '\033[104m'
+    BBR_MAGENTA = '\033[105m'
+    BBR_CYAN = '\033[106m'
+
+LOGO = '''
+ ____      ____  _  _  __ _  __ _  ____  ____ 
+(  _ \ ___(  _ \/ )( \(  ( \(  ( \(  __)(  _ \ 
+ ) __/(___))   /) \/ (/    //    / ) _)  )   / 
+(__)      (__\_)\____/\_)__)\_)__)(____)(__\_) 
+'''
+
+PROMPT = '    ' + ansi.DIM + '>' + ansi.ENDC
+
+# Code du module
+def main():
+    '''Main script. Iterates over text files.'''
+    print(40 * '░')
+    print(
+        ansi.BOLD + LOGO + ansi.ENDC 
+        + '\n version 1.0, © Lorelei Chevroulet' + 2 * '\n'
+        )
+    directory = select_dir()
+    print(ansi.BOLD + '2)  Processing file(s):' + 3 * '\n' + ansi.ENDC)
+    num = 0
+    total = str(len(list(directory.glob('*')))) 
+    for file in directory.iterdir():
+        num += 1
+        print(
+            2 * (ansi.UP + ansi.CLINE)
+            + f'    {ansi.BR_BLUE}{num:6}{ansi.ENDC}/{total:6} | File name\n'
+            + 20 * ' ' + file.name
+            )
+        time.sleep(0)
+        if not file.name == '.DS_Store':
+            content = import_text(file)
+            export = html_slice(content)
+            if export is not None:
+                export_text(out_directory, export, file.name)
+
+def select_dir():
+    print(ansi.BOLD + '1)  Enter text files directory:\n' + ansi.ENDC)
+    directory = Path(input(PROMPT).strip().replace("\\", ""))
+    print(f'\n    Looking for \'{ansi.BR_BLUE}{directory}{ansi.ENDC}\'...')
+    if not directory.exists():
+        print('\n' + ansi.FAIL + ansi.BOLD
+            + f'    Path \'{ansi.BR_BLUE}{directory}{ansi.FAIL}\' not found.'
+            + ansi.ENDC
+        )
+        sys.exit()
+    print(
+        '\n    '
+        + ansi.BR_GREEN + ansi.BOLD
+        + str(len(list(directory.glob('*')))) 
+        + ' file(s) found'
+        + ansi.ENDC
+        +'. Continue? y/n \n'
+        )
+    if input(PROMPT) == 'y':
+        print('\n')
+        return directory
+    else:
+        sys.exit()
+
+def select_out_dir():
+    print(ansi.BOLD + '2)  Enter output directory:\n' + ansi.ENDC)
+    out_dir = Path(input(PROMPT).strip().replace("\\", ""))
+    print(f'\n    Looking for \'{ansi.BR_BLUE}{out_dir}{ansi.ENDC}\'...')
+    if not out_dir.exists():
+        print('\n' + ansi.FAIL + ansi.BOLD
+            + f'    Path \'{ansi.BR_BLUE}{out_dir}{ansi.FAIL}\' not found.'
+            + ansi.ENDC
+        )
+        sys.exit()
+    print(
+        '\n    '
+        + ansi.BR_GREEN + ansi.BOLD
+        + 'Directory found'
+        + ansi.ENDC
+        +'. Continue? y/n \n'
+        )
+    if input(PROMPT) == 'y':
+        print('\n')
+        return out_dir
+    else:
+        sys.exit()
+
+def import_text(file):
+    try:
+        with file.open('r', encoding='utf-8') as file:
+            return file.read()
+    except Exception:
+        print(
+            ansi.FAIL + ansi.BOLD
+            + 'Error while accessing file.'
+            + ansi.ENDC
+            )
+        sys.exit()
+
+def filter_text():
+    return
+
+def export_text(out_dir, export, filename):
+    file = Path(out_dir/filename)
+    try:
+        with file.open('w', encoding='utf-8') as f:
+            f.write(export)
+    except Exception:
+        print('woops!')
+        return
+    return
+
+def html_slice(content):
+    i = 0
+    j = 0
+    try:
+        i = content.find('<h2 class="heading-size-3">Description</h2>')
+    except Exception:
+        return
+    if i == -1:
+        return
+    test = content[i+43:len(content)]
+    j = test.find('<h2')
+    test2 = test[0:j]
+    while test2.find('<') > 0:
+        k = test2.find('<')
+        l = test2.find('>')
+        test2 = test2[0:k] + test2[l+1:len(test2)]
+        time.sleep(0)
+    return test2
+
+# End-of-file (EOF)
+if __name__ == "__main__":
+    main()
